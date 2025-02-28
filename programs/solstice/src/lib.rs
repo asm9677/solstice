8bca646d2a6f8bd9f8f8678fef8c389db5d3c83a
6luv <jmj00005@naver.com>
2025-03-01 04:28:51 +0900

    feat: create card

--- programs/solstice/src/lib.rs
+++ programs/solstice/src/lib.rs
@@ -6,15 +6,38 @@ declare_id!("3yC1TZXgWjH25ULMC1ibj8csVdArPXFKt6tNAnqUqBLt");
 pub mod solstice {
     use super::*;
 
-    pub fn imageChunkTransaction(_ctx: Context<ImageChunkTransactionAccounts>, data: Vec<u8>, hash1: Option<Pubkey>, hash2: Option<Pubkey>) -> Result<String> {
-        msg!("Received data length: {}", data.len());
-        // msg!("Received key1: {}", hash1.unwrap());
-        // msg!("Received key2: {}", hash2.unwrap());
-        Ok(data.len().to_string())
+    pub fn image_chunk_transaction(_ctx: Context<ImageChunkTransactionAccounts>, _chunk_data: Vec<u8>, _child_hash1: String, _child_hash2: String) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn create_card(ctx: Context<CardAccounts>, owner: Option<Pubkey>, latest_image_tx: String) -> Result<()> {
+        ctx.accounts.card_account.owner = owner.unwrap();
+        ctx.accounts.card_account.latest_image_tx = latest_image_tx;
+        Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct ImageChunkTransactionAccounts<'info> {
     pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct CardAccounts<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 33 + 88,
+    )]
+    pub card_account: Account<'info, Card>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Card {
+    owner: Pubkey,
+    latest_image_tx: String,
 }
--- tests/solstice.ts
+++ tests/solstice.ts
@@ -1,27 +1,53 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Solstice } from "../target/types/solstice";
-import { PublicKey } from "@solana/web3.js";
+import { Keypair, SystemProgram } from "@solana/web3.js";
+import fs from "fs";
+import { expect } from "chai";
 
 describe("solstice", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Solstice as Program<Solstice>;
 
-  it("Image chunk transaction", async () => {
+  it("Image Chunk Transaction", async () => {
     const user = anchor.web3.Keypair.generate();
 
-    const testData = Buffer.from([1, 2, 3, 4, 5]); // 바이너리 데이터
+    const testData = Buffer.from([1, 2, 3, 4, 5]);
     const hash1 = null;
     const hash2 = null;
 
     const tx = await program.methods
       .imageChunkTransaction(testData, hash1, hash2)
       .accounts({
-        signer: user.publicKey, // 유저 계정
+        signer: user.publicKey,
       })
-      .signers([user]) // 서명자 추가
+      .signers([user])
       .rpc();
+  });
+
+  it("Create Card", async () => {
+    const secretKey = Uint8Array.from(
+      JSON.parse(
+        fs.readFileSync(`${process.env.HOME}/.config/solana/id.json`, "utf8")
+      )
+    );
+    const owner = Keypair.fromSecretKey(secretKey);
+    const latest_image_tx = owner;
+    const payer = owner;
+    const cardAccount = Keypair.generate();
+
+    const tx = await program.methods
+      .createCard(owner.publicKey, latest_image_tx.publicKey)
+      .accounts({
+        cardAccount: cardAccount.publicKey,
+        user: payer.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([payer, cardAccount])
+      .rpc();
+
+    const accountData = await program.account.card.fetch(cardAccount.publicKey);
+    expect(accountData.owner.toString()).to.equal(owner.publicKey.toString());
   });
 });
