89451067f90ecdbd5a5d2526ea22b9914caa60b8
6luv <jmj00005@naver.com>
2025-03-01 02:50:35 +0900

    feat: transaction program

--- programs/solstice/src/lib.rs
+++ programs/solstice/src/lib.rs
@@ -1,16 +1,20 @@
 use anchor_lang::prelude::*;
 
-declare_id!("EsjGSxfqv9jtBDWmUpPypj58uatCaqppPD7sCULujXAu");
+declare_id!("3yC1TZXgWjH25ULMC1ibj8csVdArPXFKt6tNAnqUqBLt");
 
 #[program]
 pub mod solstice {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn imageChunkTransaction(_ctx: Context<ImageChunkTransactionAccounts>, data: Vec<u8>, hash1: Option<Pubkey>, hash2: Option<Pubkey>) -> Result<String> {
+        msg!("Received data length: {}", data.len());
+        // msg!("Received key1: {}", hash1.unwrap());
+        // msg!("Received key2: {}", hash2.unwrap());
+        Ok(data.len().to_string())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct ImageChunkTransactionAccounts<'info> {
+    pub signer: Signer<'info>,
+}
--- tests/solstice.ts
+++ tests/solstice.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Solstice } from "../target/types/solstice";
+import { PublicKey } from "@solana/web3.js";
 
 describe("solstice", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,19 @@ describe("solstice", () => {
 
   const program = anchor.workspace.Solstice as Program<Solstice>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Image chunk transaction", async () => {
+    const user = anchor.web3.Keypair.generate();
+
+    const testData = Buffer.from([1, 2, 3, 4, 5]); // 바이너리 데이터
+    const hash1 = null;
+    const hash2 = null;
+
+    const tx = await program.methods
+      .imageChunkTransaction(testData, hash1, hash2)
+      .accounts({
+        signer: user.publicKey, // 유저 계정
+      })
+      .signers([user]) // 서명자 추가
+      .rpc();
   });
 });
