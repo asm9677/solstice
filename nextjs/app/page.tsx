6d413fbf0d2028f485f5b9dad25ae08a18656f97
YeonhaPark <yonaprisca@gmail.com>
2025-03-01 11:43:01 +0900

    fix: vercel -> nextjs 로 변경

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,7 @@
+import {Canvas} from "@/components/canvas";
+
+export default function Home() {
+  return (
+      <Canvas></Canvas>
+  );
+}
---
+++ nextjs/types/index.ts
@@ -0,0 +1,12 @@
+interface TextBox {
+    id: string;
+    x: number;
+    y: number;
+    text: string;
+    fontSize: number;
+    fill: string;
+    draggable: boolean;
+
+}
+
+export type { TextBox }
